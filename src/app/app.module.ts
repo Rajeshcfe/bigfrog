68e095543fb19fa8c0791974311e1f203e94b384
ajay <ajay123maurya456@gmail.com>
2023-02-15 23:26:03 +0530

    all changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExploreSectionComponent } from './explore-section/explore-section.component';
+import { FooterSectionComponent } from './footer-section/footer-section.component';
+import { HeaderAreaComponent } from './header/header-area/header-area.component';
+import { KidsSectionComponent } from './kids-section/kids-section.component';
+import { MenSectionComponent } from './men-section/men-section.component';
+import { SocialMediaSectionComponent } from './social-media-section/social-media-section.component';
+import { SubscribeSectionComponent } from './subscribe-section/subscribe-section.component';
+import { TopSectionComponent } from './top-section/top-section.component';
+import { WomenSectionComponent } from './women-section/women-section.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderAreaComponent,
+    TopSectionComponent,
+    MenSectionComponent,
+    FooterSectionComponent,
+    WomenSectionComponent,
+    KidsSectionComponent,
+    ExploreSectionComponent,
+    SocialMediaSectionComponent,
+    SubscribeSectionComponent
   ],
   imports: [
     BrowserModule,
